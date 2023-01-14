f7fd603494bc61af58ff503fddeb6afc56b906fb
jlcudris <weiselcudris@hotmail.com>
2023-01-14 18:07:51 -0500

    primera actividad creando slide y agregando css y modificando nuestros propios slides

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  slideOpt ={
+    initialSlide:0,
+    slidesPerview:1,
+    centerSlides:true,
+    speed:400
+  }
+
+  slides_nuevos=[
+    {
+      title:"The game of thrones",
+      img:"assets/gamet.jpg",
+      descripcion:"  El libro mejor vendido de la serie A Song of Ice and Fire escrito por George R.R, es llevado a la pantalla chica cuando HBO decide recrear esta historia épica de fantasía"
+    },
+    {
+      title:"Harry Potter",
+      img:"assets/harry.jpg",
+      descripcion:" El día de su cumpleaños, Harry Potter descubre que es hijo de dos conocidos hechiceros, de los que ha heredado poderes mágicos."
+    }
+  ]
+
   constructor() {}
 
 }
